6f110774ae568642aa6207ec3ec6ae826a2b84ee
shivareddy17 <shivareddych17@gmail.com>
2024-11-21 01:30:20 +0530

    add todo methods

--- routes/todos.js
+++ routes/todos.js
@@ -1,12 +1,70 @@
 const {Router} =require("express");
-
+const {authmidlleware}=require("../authmiddlewares")
 const todoRouter=Router();
-
-todoRouter.get("/",(req,res)=>{
+const {todoModel}=require("../db")
+todoRouter.get("/",authmidlleware,async(req,res)=>{
+    const id=req.id;
+    console.log(id)
+    try{
+    const todos=await todoModel.find({userId:id})
     res.json({
-        msg:"todos end point called"
+       
+        todos
     })
+}catch(e){
+    console.log(e)
+    res.json({
+        msg:"error finding todos"
+    })
+}
 })
+todoRouter.post("/addtodo",authmidlleware,async (req,res)=>{
+   const id=req.id
+   console.log(id)
+   const {todo}=req.body
+   try{
+     const todos=await todoModel.create({
+        todo,
+        done:false,
+        userId:id
+
+     })
+     res.json({
+        msg:"todo created",
+        id:todos._id
+     })}catch(e){
+        console.log(e)
+        res.json({
+            msg:"error accoured while creating a todo"
+        })
+     }
+})
+todoRouter.put("/:id",authmidlleware,async(req,res)=>{
+    const {id}=req.params
+    const{todo,done}=req.body;
+    console.log(id,todo,done);
+    try{
+    const course = await todoModel.updateOne({
+        _id: id 
+        
+    }, {
+        todo:todo, 
+        done:done, 
+        
+    })
+    console.log(course._id)
+    res.json({
+        message: "Course updated",
+        courseId: course._id
+    })}catch(e){
+        console.log(e)
+        res.json({
+            msg:"error updating todo"
+        })
+    }
+})
+
+
 
 
 module.exports={
